e6e59b85729d4de34726b4e37796bd5ef4552e4e
Liam Snow <liamsnow03@gmail.com>
2023-05-23 18:45:39 -0400

    Working on Full Console Display

--- blackjack.cs
+++ blackjack.cs
@@ -1,17 +1,39 @@
 ﻿using System;
+using System.Text;
 
 class BlackJack
 {
     public static void Main(string[] args)
     {
+        Display display = new Display(ConsoleColor.DarkBlue, ConsoleColor.Gray);
+
+        //Draw Edges
+        for (int y = 1; y < display.height - 2; y++)
+        {
+            display.WriteAt("██", 1, y);
+            display.WriteAt("██", display.width - 3, y);
+            display.WriteCentered("█", y);
+        }
+        StringBuilder longEdgeBuilder = new StringBuilder();
+        for (int x = 1; x < display.width - 1; x++)
+        {
+            longEdgeBuilder.Append("█");
+        }
+        string longEdge = longEdgeBuilder.ToString();
+        display.WriteAt(longEdge, 1, 1);
+        display.WriteAt(longEdge, 1, display.height - 2);
+
+        //Draw Title
+        display.WriteCentered("██ BLACKJACK ██", 2);
+        display.WriteCentered("███████████████", 3);
+
+        
+
         bool programRunning = true;
 
         //Game Sequence
         while (programRunning)
         {
-            //intro
-            Console.WriteLine("<<< BLACKJACK >>>\n");
-
             //create deck
             Deck deck = new Deck();
 
@@ -19,90 +41,93 @@ class BlackJack
             Hand playerHand = new Hand();
             playerHand.add(deck.drawRandom());
             playerHand.add(deck.drawRandom());
-            Console.WriteLine("Your current hand = {0}", string.Join(",", playerHand.allValues));
-            Console.Write(playerHand.ToString());
+            //Console.WriteLine("Your current hand = {0}", string.Join(",", playerHand.allValues));
+            //Console.Write(playerHand.ToString());
 
             //create dealer hand
             Hand dealerHand = new Hand();
             dealerHand.add(deck.drawRandom());
             dealerHand.add(deck.drawRandom());
-            Console.WriteLine("The dealers hand is");
-            Console.Write(dealerHand.ToString(true));
+            //Console.WriteLine("The dealers hand is");
+            //Console.Write(dealerHand.ToString(true));
 
-            //Player Hitting Loop
-            while (!dealerHand.hasAutoWon && !playerHand.hasAutoWon)
-            {
-                Console.WriteLine("\nWould you like to HIT (h) or STAND (s)?");
-                char hitStandResponse = Console.ReadKey(true).KeyChar;
-                if (Char.ToLower(hitStandResponse) == 'h')
-                {
-                    playerHand.add(deck.drawRandom());
-                    Console.WriteLine("HIT -> Your current hand is now = {0}", string.Join(",", playerHand.allValues));
-                    Console.Write(playerHand.ToString());
-                    if (playerHand.hasBusted)
-                    {
-                        break;
-                    }
-                }
-                else if (Char.ToLower(hitStandResponse) == 's')
-                {
-                    Console.WriteLine("STAND");
-                    break;
-                }
-            }
+            Thread.Sleep(10000000);
 
-            //Dealer Hitting Loop
-            Console.WriteLine("");
-            while (!playerHand.hasAutoWon && !playerHand.hasBusted && !dealerHand.hasAutoWon && dealerHand.highestValidValue < 17 && !dealerHand.hasBusted) 
-            {
-                dealerHand.add(deck.drawRandom());
-                Console.WriteLine("DEALER HIT", string.Join(",", dealerHand.allValues));
-            }
-            if (!playerHand.hasAutoWon && !playerHand.hasBusted && !dealerHand.hasBusted) {
-                Console.WriteLine("DEALER STANDS AT {0}", string.Join(",", dealerHand.allValues));
-                Console.Write(dealerHand.ToString());
-            }
+            ////Player Hitting Loop
+            //while (!dealerHand.hasAutoWon && !playerHand.hasAutoWon)
+            //{
+            //    Console.WriteLine("\nWould you like to HIT (h) or STAND (s)?");
+            //    char hitStandResponse = Console.ReadKey(true).KeyChar;
+            //    if (Char.ToLower(hitStandResponse) == 'h')
+            //    {
+            //        playerHand.add(deck.drawRandom());
+            //        Console.WriteLine("HIT -> Your current hand is now = {0}", string.Join(",", playerHand.allValues));
+            //        Console.Write(playerHand.ToString());
+            //        if (playerHand.hasBusted)
+            //        {
+            //            break;
+            //        }
+            //    }
+            //    else if (Char.ToLower(hitStandResponse) == 's')
+            //    {
+            //        Console.WriteLine("STAND");
+            //        break;
+            //    }
+            //}
 
-            //Log Solution
-            Console.WriteLine("");
-            if (playerHand.hasBusted)
-            {
-                Console.WriteLine("YOU BUST :(((");
-            }
-            else if (dealerHand.hasBusted)
-            {
-                Console.WriteLine("DEALER BUST -> YOU WIN!!");
-            }
-            else if (playerHand.highestValidValue == dealerHand.highestValidValue) 
-            {
-                Console.WriteLine("TIE GAME - NO WINNER");
-            }
-            else if (playerHand.highestValidValue > dealerHand.highestValidValue)
-            {
-                Console.WriteLine("YOU WON!!! {0} > {1}", playerHand.highestValidValue, dealerHand.highestValidValue);
-            }
-            else
-            {
-                Console.WriteLine("YOU LOST :(( {0} < {1}", playerHand.highestValidValue, dealerHand.highestValidValue);
-            }
+            ////Dealer Hitting Loop
+            //Console.WriteLine("");
+            //while (!playerHand.hasAutoWon && !playerHand.hasBusted && !dealerHand.hasAutoWon && dealerHand.highestValidValue < 17 && !dealerHand.hasBusted)
+            //{
+            //    dealerHand.add(deck.drawRandom());
+            //    Console.WriteLine("DEALER HIT", string.Join(",", dealerHand.allValues));
+            //}
+            //if (!playerHand.hasAutoWon && !playerHand.hasBusted && !dealerHand.hasBusted)
+            //{
+            //    Console.WriteLine("DEALER STANDS AT {0}", string.Join(",", dealerHand.allValues));
+            //    Console.Write(dealerHand.ToString());
+            //}
 
-            //Play Again
-            while (true)
-            {
-                Console.Write("\nWould you like to play again? (y/n): ");
-                char playAgainResponse = Console.ReadKey(true).KeyChar;
-                Console.WriteLine("");
-                if (Char.ToLower(playAgainResponse) == 'n')
-                {
-                    programRunning = false;
-                    break;
-                }
-                else if (Char.ToLower(playAgainResponse) == 'y')
-                {
-                    Console.Clear();
-                    break;
-                }
-            }
+            ////Log Solution
+            //Console.WriteLine("");
+            //if (playerHand.hasBusted)
+            //{
+            //    Console.WriteLine("YOU BUST :(((");
+            //}
+            //else if (dealerHand.hasBusted)
+            //{
+            //    Console.WriteLine("DEALER BUST -> YOU WIN!!");
+            //}
+            //else if (playerHand.highestValidValue == dealerHand.highestValidValue)
+            //{
+            //    Console.WriteLine("TIE GAME - NO WINNER");
+            //}
+            //else if (playerHand.highestValidValue > dealerHand.highestValidValue)
+            //{
+            //    Console.WriteLine("YOU WON!!! {0} > {1}", playerHand.highestValidValue, dealerHand.highestValidValue);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("YOU LOST :(( {0} < {1}", playerHand.highestValidValue, dealerHand.highestValidValue);
+            //}
+
+            ////Play Again
+            //while (true)
+            //{
+            //    Console.Write("\nWould you like to play again? (y/n): ");
+            //    char playAgainResponse = Console.ReadKey(true).KeyChar;
+            //    Console.WriteLine("");
+            //    if (Char.ToLower(playAgainResponse) == 'n')
+            //    {
+            //        programRunning = false;
+            //        break;
+            //    }
+            //    else if (Char.ToLower(playAgainResponse) == 'y')
+            //    {
+            //        Console.Clear();
+            //        break;
+            //    }
+            //}
         }
     }
 }
---
+++ display.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+public class Display
+{
+
+    public int cursorTop, cursorLeft;
+    public int width { get { return Console.WindowWidth; } }
+    public int height { get { return Console.WindowHeight; } }
+
+    public void WriteAt(string str, int x, int y)
+    {
+        try
+        {
+            Console.SetCursorPosition(cursorLeft + x, cursorTop + y);
+            Console.Write(str);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.Clear();
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public void WriteCentered(string str, int y) {
+        WriteAt(str, (width - str.Length) / 2, y);
+    }
+
+    public void ClearScreen()
+    {
+        StringBuilder blankScreenBuilder = new StringBuilder();
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                blankScreenBuilder.Append(" ");
+            }
+        }
+        WriteAt(blankScreenBuilder.ToString(), 0, 0);
+    }
+
+    public Display(ConsoleColor backgroundColor, ConsoleColor foregroundColor) {
+        Console.Clear();
+        cursorTop = Console.CursorTop;
+        cursorLeft = Console.CursorLeft;
+
+        Console.CursorVisible = false;
+        Console.BackgroundColor = backgroundColor;
+        Console.ForegroundColor = foregroundColor;
+        ClearScreen();
+    }
+}
